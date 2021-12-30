eaf1bd95e8ca299526117a0707682dcea87af414
Mousero141 <Grafy06@gmail.com>
2021-12-30 14:34:31 +0100

    update

--- src/main/java/org/but/feec/esport/Main.java
+++ src/main/java/org/but/feec/esport/Main.java
@@ -2,8 +2,6 @@ package org.but.feec.esport;
 
 public class Main {
     public static void main(String[] args) {
-        String test = "inp";
         App.main(args);
-        System.out.println("Ahoj tady tohle je nejaka stracka" + test + ";" );
     }
 }
